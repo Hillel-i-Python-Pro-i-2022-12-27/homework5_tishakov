3073861131904e68330db7053f84c6dbf1be9ca5
daniel <tishakovdanyilgmail.com>
2023-01-24 16:23:44 +0000

    create def in main

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-# This is a sample Python script.
+import requests
+import json
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from faker import Faker
+from applications.PATH.path import FILES_INPUT_FILES
+from reader import Reader
+
+def read_text():
+    data = FILES_INPUT_FILES
+    with open(data) as a:
+        reading = a.read()
+    return reading
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+fake = Faker(["en_US"])
+def generator_email():
+    data = []
+    for _ in range(0, 100):
+        data.append(fake.email())
+    return data
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def get_info():
+    response = requests.get("http://api.open-notify.org/astros.json")
+    with open("info", "w") as file:
+        file.write(response.text)
+    answer = json.loads(response.text)
+    for i in answer["people"]:
+        print(i["name"])
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def csv_reader():
+    text = "people.csv"
+    with Reader.readFromFile(open(text, newline="")) as file:
+        for line in file:
+            print(line)
+    # with Reader.openWithName(text) as file:
+    #     reader = csv.Reader()
+    #     haeders = next[reader]
+    #     print("Headers: ", haeders)
+    #     for row in reader:
+    #         print(row)
+def main():
+    print("\tзадание 1 чтенние файла по пути\n", read_text())
+    print("\tзадание 2 сгенерировать 100 мэйлов\n", generator_email())
+    print("\tзадание 3 вывести космонавтов\n", get_info())
+    print(csv_reader())
+
+if __name__=="__main__":
+    main()
