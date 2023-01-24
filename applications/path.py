f573bb59c622074fddb8ed44730da11d1a594bc3
daniel <tishakovdanyilgmail.com>
2023-01-24 15:56:50 +0000

    add requirements, gitignore, folders

---
+++ applications/path.py
@@ -0,0 +1,9 @@
+from pathlib import Path
+from typing import Final
+
+
+ROOT_PATH: Final[Path] = Path(__file__).parents[2]
+FILES_INPUT_FILES: Final[Path] = ROOT_PATH.joinpath("files_input", "text.txt")
+FILES_OUTPUT_FILES: Final[Path] = ROOT_PATH.joinpath("output_input")
+
+print(FILES_INPUT_FILES)
