e396251673a13ae732433bf7995bfa20315c6238
daniel <tishakovdanyilgmail.com>
2023-01-26 13:00:25 +0000

    complete

--- application/config/paths.py
+++ application/config/paths.py
@@ -5,6 +5,3 @@ from typing import Final
 ROOT_PATH: Final[Path] = Path(__file__).parents[2]
 FILES_INPUT_FILES: Final[Path] = ROOT_PATH.joinpath("files_input", "text.txt")
 FILES_INPUT_FILES_CSV: Final[Path] = ROOT_PATH.joinpath("files_input", "people.csv")
-FILES_OUTPUT_FILES: Final[Path] = ROOT_PATH.joinpath("output_input")
-
-print(ROOT_PATH)
--- application/home_works_examples/astronaut_len.py
+++ application/home_works_examples/astronaut_len.py
@@ -1,14 +1,14 @@
 import requests
-import json
 
 
 def austronaut_len():
     response = requests.get("http://api.open-notify.org/astros.json")
-    with open("info", "w") as file:
-        file.write(response.text)
-    answer = json.loads(response.text)
-    for i in answer["people"]:
-        print(i["name"])
+    data = response.json()
+    answer = []
+    while True:
+        for i in data["people"]:
+            answer.append(i["name"])
+        return answer
 
 
 print(austronaut_len())
